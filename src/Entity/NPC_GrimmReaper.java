1efc2ec10062aa64dd41e654eac4784b0f416852
julietteberkhout <s1120749@student.hsleiden.nl>
2024-01-04 15:03:31 +0100

    Grimm dialogen aangepast

--- src/Entity/NPC_GrimmReaper.java
+++ src/Entity/NPC_GrimmReaper.java
@@ -28,9 +28,9 @@ public class NPC_GrimmReaper extends Entity {
     }
 
     public void setDialogue() {
-        dialogues[0] = "Hello my dear ^^\n\nOh my, I am still in my... Euh... Friday robes!\nOh well...You don't mind " +
+        dialogues[0] = "Hello my dear ^^\nOh my, I am still in my... Euh... Friday robes!\nOh well...You don't mind " +
                 "do you?";
-        dialogues[1] = "Ah, that's right, you probably have some questions\non were you are and how you got here" +
+        dialogues[1] = "Ah, that's right, you probably have some questions on were you are and how you got here" +
                 "\nI'll explain them for you!";
         dialogues[2] = "So... Were do I start?";
         dialogues[3] = "...Euhm...Well...";
@@ -45,18 +45,17 @@ public class NPC_GrimmReaper extends Entity {
                 "sounds like.\nYou also have some lovely people waiting for you! <3";
         dialogues[12] = "But that's not the reason we came to this island specifically...";
         dialogues[13] = "The thing is, I have been doing this for a very, very, very long time and have gotten really " +
-                "tired. I haven't even been able to attend my knitting club in decennia!\nReading books, something I " +
-                "used to love, has become a chore...\nAnd let's nog even start about my my poor, poor Ceberus... " +
-                "Sitting at home... Alone... All day!:(\nEven getting dressed out of my pyjama's in the morning is " +
-                "difficult!";
-        dialogues[14] = "...";
-        dialogues[15] = "...Euh... Yeah...";
-        dialogues[16] = "These are actually my pyjama robes...";
-        dialogues[17] = "Anyway, the thing I am asking trying to say, is that I am looking for a new successor.";
-        dialogues[18] = "But I want it to be someone who is loving and caring,\nsome characteristics I think you might " +
+                "tired.\nI haven't even been able to attend my knitting club in decennia!\nReading books, something I " +
+                "used to love, has become a chore...\nAnd let's nog even start about my poor, poor Ceberus... " +
+                "Sitting at home... Alone... All day!:(";
+        dialogues[14] = "Even getting dressed out of my pyjama's in the morning is difficult!";
+        dialogues[15] = "...";
+        dialogues[16] = "...Yeah\nThese are actually my pyjama robes...";
+        dialogues[17] = "Anyway, the thing I am trying to say, is that I am looking for a new successor.";
+        dialogues[18] = "But I want it to be someone who is loving and caring, some characteristics I think you might\n" +
                 "posses:)";
-        dialogues[19] = "To test my new successor I made this island, containing 3 dungeons\nto challenge if " +
-                "you are the right person!\n\nSo if you are intrested, I'd say give it a go!:)";
+        dialogues[19] = "To test my new successor I made this island, containing 3 dungeons to see if " +
+                "you are the right\nperson!\nSo if you are intrested, I'd say give it a go!:)";
     }
 
     public void speak() {
