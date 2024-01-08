458b50a04103e4d6005e25437b9c37d534ec31f9
julietteberkhout <s1120749@student.hsleiden.nl>
2024-01-08 23:49:20 +0100

    -Dialogen outline beetje aangepast

--- src/Entity/NPC_GrimmReaper.java
+++ src/Entity/NPC_GrimmReaper.java
@@ -51,17 +51,18 @@ public class NPC_GrimmReaper extends Entity {
         dialogues[13] = """
                 The thing is, I have been doing this for a very, very, very long time and have gotten really tired.
                 I haven't even been able to attend my knitting club in centuries!
-                Reading books, something I used to love, has become a chore...
-                And let's nog even start about my poor, poor Ceberus... Sitting at home... Alone... All day!:(""";
-        dialogues[14] = "Even getting dressed out of my pyjama's in the morning is difficult!";
+                Reading books, something I used to love, has become a chore...""";
+        dialogues[14] = """
+                        And let's nog even start about my poor, poor Ceberus... Sitting at home... Alone... All day!:(
+                        Even getting dressed out of my pyjama's in the morning is difficult!""";
         dialogues[15] = "...";
         dialogues[16] = "...Yeah\nThese are actually my pyjama robes...";
         dialogues[17] = "Anyway, the thing I am trying to say, is that I am looking for a new successor.";
         dialogues[18] = "But I want it to be someone who is loving and caring, some characteristics I think you might\n" +
                 "posses:)";
         dialogues[19] = """
-                To test my new successor I made this island, containing 3 dungeons to see if you are the right
-                person!
+                To test my new successor I made this island, containing 3 dungeons to see if you are the 
+                right person!
                 So if you are intrested, I'd say give it a go!:)""";
         dialogues[20] = "Here is the key for the first dungeon";
     }
