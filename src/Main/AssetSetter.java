d37630e971f16d48517f02ac9772c6edf00198e9
julietteberkhout <s1120749@student.hsleiden.nl>
2024-01-10 19:06:09 +0100

    -Damage pit -Healing pool

--- src/Main/AssetSetter.java
+++ src/Main/AssetSetter.java
@@ -46,7 +46,7 @@ public class AssetSetter {
     public void setNPC() {
         gp.npc[0] = new NPC_GrimmReaper(gp);
         gp.npc[0].worldX = gp.tileSize * 37;
-        gp.npc[0].worldY = gp.tileSize * 45;
+        gp.npc[0].worldY = gp.tileSize * 35;
 
         gp.npc[1] = new NPC_1(gp);
         gp.npc[1].worldX = gp.tileSize * 37;
